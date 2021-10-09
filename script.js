81abf6dd23e05d20091303bb294990f2514fa261
Liya <theresliya@gmail.com>
2021-10-09 23:52:13 +0530

    changes in UI

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-var txt = "";
+let txt = "";
+let count=0;
 function list_additem() {
+  count++;
   let input = document.getElementById("input_notes");
   if (input.value.trim() === "") {
     window.alert("Enter any value");
     return;
   }
-  txt += "<li>" + input.value + "</li>";
+  txt += "<li>"+ count+". "+ input.value +"<div>"+"<button class="+"edit_button"+"><img src="+"./images/icons8-edit-24.png"+"></button>"+"<button><img src="+"./images/icons8-trash-can-50.png"+"></button>"+"<input type="+"checkbox"+">"+"</div>"+"</li>";
   document.getElementById("printing_list").innerHTML = txt;
   input.value = "";
 }
