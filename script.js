666a18182ab6711a39ac50761458619c0096d25d
Liya <theresliya@gmail.com>
2021-10-28 00:55:03 +0530

    checkbox functionalities

--- script.js
+++ script.js
@@ -1,31 +1,54 @@
-let txt = "";
-let count=0,itemNumber=0;
-let list= [];
+let count = 0, itemNumber = 0;
+let list = [];
 
+
+//Function to add item to to-do list
 function list_additem() {
   let input = document.getElementById("input_notes");
 
   list.push({
-    id: count++ ,
+    id: count++,
     description: input.value,
     taskCompleted: false
   });
-  
+
+  //modal display on empty input
   if (input.value.trim() === "") {
-    document.getElementById("modal_parent").style.display= "flex";
+    document.getElementById("modal_parent").style.display = "flex";
     document.getElementById("input_notes").blur();
     return;
   }
 
-    txt = "<li>"+"<div class="+"list_content"+">"+"<input type="+"checkbox"+" class="+"checkbox"+">"+
-    "<span>"+ list[itemNumber].description +"</span>"+"</div>"+"<div class="+"buttons"+">"+"<button class="+"edit_button"+"><img src="+"./images/icons8-edit-24.png"+"></button>"+
-    "<button class="+"remove_button"+"><img src="+"./images/icons8-trash-can-50.png"+"></button>"+"</div>"+"</li>" +txt;
-    document.getElementById("printing_list").innerHTML = txt;
-    input.value = "";
-    
+  let newItem = "<li><div class='list_content'><input type='checkbox' class='checkbox' id='c" + list[itemNumber].id + "'><span id = 'd" + list[itemNumber].id + "' class='description'>" + list[itemNumber].description + "</span></div><div class='buttons'><button class='edit_button'><img src='./images/icons8-edit-24.png'></button><button class='remove_button'><img src='./images/icons8-trash-can-50.png'></button></div></li>";
+
+  document.getElementById("printing_list").insertAdjacentHTML('afterbegin', newItem);
+  input.value = "";
+
+
+  let checkBox = document.getElementById("c" + list[itemNumber].id)
+  let description = document.getElementById("d" + list[itemNumber].id)
+
+  //checkbox functionalities
+  function toggleCheckedClass(){
+    if (checkBox.checked) {
+      description.classList.add("checkboxChecked");
+    }
+    else {
+      description.classList.remove("checkboxChecked");
+    }
+  }
+
+  checkBox.addEventListener("change", toggleCheckedClass)
+  
+  description.addEventListener("click", function () {
+    checkBox.checked = checkBox.checked ? false : true;
+    toggleCheckedClass();
+  })
+
   itemNumber++;
 }
 
+//focus on load and submission of input on enter
 window.addEventListener("load", function () {
   document.getElementById("input_notes").focus();
   document.getElementById("input_notes").addEventListener("keydown", function (e) {
@@ -34,7 +57,10 @@ window.addEventListener("load", function () {
     }
   });
 });
-function close_modal(){
-  document.getElementById("modal_parent").style.display= "none";
+
+// function to close modal
+function close_modal() {
+  document.getElementById("modal_parent").style.display = "none";
   document.getElementById("input_notes").focus();
 }
+
