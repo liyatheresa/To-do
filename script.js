80354b3e94e2ae1457a1c79ee3b8e847e8ee442b
Liya <theresliya@gmail.com>
2021-10-29 01:55:16 +0530

    fixed error of double submission

--- script.js
+++ script.js
@@ -4,73 +4,72 @@ let list = [];
 
 //Function to add item to to-do list
 function list_additem() {
-  let input = document.getElementById("input_notes");
-  let date=new Date(Date.now()).toLocaleDateString();
-  list.push({
-    id: count++,
-    description: input.value,
-    taskCompleted: false
-  });
+	console.log("vannu")
+	let input = document.getElementById("input_notes");
+	let date = new Date(Date.now()).toLocaleDateString();
+	list.push({
+		id: count++,
+		description: input.value,
+		taskCompleted: false
+	});
 
-  //modal display on empty input
-  if (input.value.trim() === "") {
-    document.getElementById("modal_parent").style.display = "flex";
-    document.getElementById("input_notes").blur();
-    return;
-  }
+	//modal display on empty input
+	if (input.value.trim() === "") {
+		document.getElementById("modal_parent").style.display = "flex";
+		document.getElementById("input_notes").blur();
+		return;
+	}
 
-  let newItem = "<li><div class='list_content'>"+
-                "<input type='checkbox' class='checkbox' id='c" + list[itemNumber].id + "'>"+
-                "<span class='descriptionAndDate'><span id = 'd" + list[itemNumber].id + "' class='description'>" + list[itemNumber].description + "</span>"+
-                "<div class='descriptionDate'>"+date+"</div></span>"+
-                "</div>"+
-                "<div class='buttons'>"+
-                "<button class='edit_button'><img src='./images/icons8-edit-24.png'></button>"+
-                "<button class='remove_button'><img src='./images/icons8-trash-can-50.png'></button>"+
-                "</div></li>";
+	let newItem = "<li><div class='list_content'>" +
+		"<input type='checkbox' class='checkbox' id='c" + list[itemNumber].id + "'>" +
+		"<span class='descriptionAndDate'><span id = 'd" + list[itemNumber].id + "' class='description'>" + list[itemNumber].description + "</span>" +
+		"<div class='descriptionDate'>" + date + "</div></span>" +
+		"</div>" +
+		"<div class='buttons'>" +
+		"<button class='edit_button'><img src='./images/icons8-edit-24.png'></button>" +
+		"<button class='remove_button'><img src='./images/icons8-trash-can-50.png'></button>" +
+		"</div></li>";
 
-  document.getElementById("printing_list").insertAdjacentHTML('afterbegin', newItem);
-  input.value = "";
-
-
-  let checkBox = document.getElementById("c" + list[itemNumber].id)
-  let description = document.getElementById("d" + list[itemNumber].id)
-
-  //checkbox functionalities
-  function toggleCheckedClass(){
-    if (checkBox.checked) {
-      description.classList.add("checkboxChecked");
-    }
-    else {
-      description.classList.remove("checkboxChecked");
-    }
-  }
-
-  checkBox.addEventListener("change", toggleCheckedClass)
-
-  description.addEventListener("click", function () {
-    checkBox.checked = checkBox.checked ? false : true;
-    toggleCheckedClass();
-  })
+	document.getElementById("printing_list").insertAdjacentHTML('afterbegin', newItem);
+	input.value = "";
 
 
+	let checkBox = document.getElementById("c" + list[itemNumber].id)
+	let description = document.getElementById("d" + list[itemNumber].id)
 
-  itemNumber++;
+	//checkbox functionalities
+	function toggleCheckedClass() {
+		if (checkBox.checked) {
+			description.classList.add("checkboxChecked");
+		}
+		else {
+			description.classList.remove("checkboxChecked");
+		}
+	}
+
+	checkBox.addEventListener("change", toggleCheckedClass)
+
+	description.addEventListener("click", function () {
+		checkBox.checked = checkBox.checked ? false : true;
+		toggleCheckedClass();
+	})
+
+	itemNumber++;
 }
 
+
 //focus on load and submission of input on enter
-window.addEventListener("load", function () {
-  document.getElementById("input_notes").focus();
-  document.getElementById("input_notes").addEventListener("keydown", function (e) {
-    if (e.key === "Enter") {
-      list_additem();
-    }
-  });
+window.addEventListener("load", function (e) {
+	document.getElementById("input_area").addEventListener("submit", e => {
+		e.preventDefault();
+		list_additem();
+	});
+	document.getElementById("input_notes").focus();
 });
 
 // function to close modal
 function close_modal() {
-  document.getElementById("modal_parent").style.display = "none";
-  document.getElementById("input_notes").focus();
+	document.getElementById("modal_parent").style.display = "none";
+	document.getElementById("input_notes").focus();
 }
 
