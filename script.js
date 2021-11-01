a5d8553735e96e44d2f859dc48423d233d7e85eb
Liya <theresliya@gmail.com>
2021-11-02 03:49:48 +0530

    bug fixes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let count = 0;
 let list = [];
-
+let editMode = false;
 
 //Function to add item to to-do list
 function list_additem() {
@@ -12,6 +12,7 @@ function list_additem() {
 	if (input.value.trim() === "") {
 		emptyModalParent.classList.add("modal_parent");
 		document.getElementById("input_notes").blur();
+		document.getElementById('modal').addEventListener("click",(e)=>{e.stopPropagation();})
 		emptyModalParent.addEventListener("click", close_modal);
 		return;
 	}
@@ -29,7 +30,7 @@ function list_additem() {
 		"<div class='descriptionDate'>" + date + "</div></span>" +
 		"</div>" +
 		"<div class='buttons'>" +
-		"<button class='edit_button'><img src='./images/icons8-edit-24.png'></button>" +
+		"<button id='e" + list[count].id + "' class='edit_button'><img class='editButtonImage' id='editimage" + list[count].id + "' src='./images/edit.png'></button>" +
 		"<button id='r" + list[count].id + "' class='remove_button'><img id='removeimage" + list[count].id + "' src='./images/icons8-trash-can-50.png'></button>" +
 		"</div></li>";
 
@@ -40,26 +41,56 @@ function list_additem() {
 	let checkBox = document.getElementById("c" + list[count].id)
 	let description = document.getElementById("d" + list[count].id)
 
-	//checkbox functionalities
+	//checkbox functionalities and marking it as complete in list
 	function toggleCheckedClass() {
 		if (checkBox.checked)
 			description.classList.add("checkboxChecked");
-		else 
+		else
 			description.classList.remove("checkboxChecked");
 		list[parseInt(checkBox.id.replace("c", ""))].taskCompleted = checkBox.checked;
 	}
 
-	checkBox.addEventListener("change", toggleCheckedClass)
+	if(editMode === false) {
+		checkBox.addEventListener("change", toggleCheckedClass)
 
-	description.addEventListener("click", function () {
-		checkBox.checked = checkBox.checked ? false : true;
-		toggleCheckedClass();
-	})
+		description.addEventListener("click", function () {
+			checkBox.checked = checkBox.checked ? false : true;
+			toggleCheckedClass();
+		})
+	}
+
+	
+	//Edit button functionalities
+	let editItem = document.getElementById("e" + list[count].id);
+	let editimage = document.getElementById("editimage" + list[count].id)
+	function editContent(e) {
+		e.stopPropagation();
+		if (editMode === false) {
+			editMode = true;
+			description.contentEditable = true;
+			description.classList.remove("description");
+			editimage.classList.add("saveButtonImage");
+			editimage.classList.remove("editButtonImage");
+			editimage.setAttribute("src", './images/save.png')
+		}
+		else {
+			editMode = false;
+			description.contentEditable = false;
+			description.classList.add("description");
+			editimage.classList.remove("saveButtonImage");
+			editimage.classList.add("editButtonImage");
+			editimage.setAttribute("src", './images/edit.png')
+		}
+		
+	}
+	editItem.addEventListener("click", editContent)
+	editimage.addEventListener("click", editContent)
 
 	//Remove button event
 	let removedItem = document.getElementById("r" + list[count].id);
 	let removeimage = document.getElementById("removeimage" + list[count].id);
 	function showDeletionModal(e) {
+		e.stopPropagation();
 		document.getElementById("input_notes").blur();
 		document.getElementById('removeModal').classList.add("modal_parent");
 		let id = e.target.id.replace("removeimage", "").replace("r", "");
@@ -67,17 +98,19 @@ function list_additem() {
 	}
 	removedItem.addEventListener("click", showDeletionModal);
 	removeimage.addEventListener("click", showDeletionModal);
-
+	
 	count++;
 }
 
 function deleteItem(e) {
 	console.log(e.target.dataset.id);
 	document.getElementById("item" + e.target.dataset.id).remove();
+	document.getElementById('removeModal').classList.remove("modal_parent");
+	document.getElementById("input_notes").focus();
 	list = list.filter(object => object.id !== parseInt(e.target.dataset.id));
 }
 
-//focus on load and submission of input on enter
+//focus on load and submission of input on enter And adding event to modal button and parent
 window.addEventListener("load", function (e) {
 	document.getElementById("input_area").addEventListener("submit", e => {
 		e.preventDefault();
@@ -88,6 +121,7 @@ window.addEventListener("load", function (e) {
 	let closeButton = document.getElementById('close');
 	let removeModalParent = document.getElementById('removeModal')
 	let confirmButton = document.getElementById('confirm');
+	document.getElementById('popup').addEventListener("click",(e)=>{e.stopPropagation();})
 	confirmButton.addEventListener("click", deleteItem);
 
 	closeButton.addEventListener("click", function () {
@@ -95,7 +129,7 @@ window.addEventListener("load", function (e) {
 		document.getElementById("input_notes").focus();
 	})
 
-	removeModalParent.addEventListener("click", function () {
+	removeModalParent.addEventListener("click", function (e) {
 		removeModalParent.classList.remove("modal_parent");
 		document.getElementById("input_notes").focus();
 	});
@@ -104,7 +138,7 @@ window.addEventListener("load", function (e) {
 });
 
 // function to close modal
-function close_modal() {
+function close_modal(e) {
 	document.getElementById("emptyModalParent").classList.remove("modal_parent");
 	document.getElementById("input_notes").focus();
 }
